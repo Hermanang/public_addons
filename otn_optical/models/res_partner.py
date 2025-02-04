7a5cef32bd468d80652c1ac8864bd9b3b029370b
Hermann <tobnang1234@gmail.com>
2025-02-04 10:09:55 +0000

    Odoo 18 module

---
+++ otn_optical/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import _, api, fields, models, tools
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    is_optometrist = fields.Boolean()
